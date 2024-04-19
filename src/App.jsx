0993d932bf0f48d24f9d5279d508c2e682608595
bela016 <isabela.seixas2014@gmail.com>
2024-04-19 14:47:10 -0300

    criando routes.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,14 @@
 import './App.css';
-import Home from './pages/Home/Home';
-import Animais from './pages/animais/Animais';
-import Fotos from './pages/Fotos/Fotos';
-import Depoimento from './pages/depoimento/Depoimento';
-import Login from './pages/login/Login';
-import {BrowserRouter as Roteador, Routes, Route} from 'react-router-dom';
-
-
+import Approutes from './routes';
 
 function App() {
 
   return (
     <>
-    
-<Roteador>
-     <Routes>
-        <Route exact path='/' Component={Home}/>
-        <Route exact path='/animais' Component={Animais}/>
-        <Route exact path='/fotos' Component={Fotos}/>
-        <Route exact path='/depoimento'Component={Depoimento}/>
-        <Route exact path='/login' Component={Login}/>
-     </Routes>
-     </Roteador>
-  
-      
-
-     
+        <Approutes/>
     </>
   );
 }
+
 
 export default App
---
+++ src/routes.jsx
@@ -0,0 +1,23 @@
+import Home from './pages/Home/Home';
+import Animais from './pages/animais/Animais';
+import Fotos from './pages/Fotos/Fotos';
+import Depoimento from './pages/depoimento/Depoimento';
+import Login from './pages/login/Login';
+import {BrowserRouter as Roteador, Routes, Route} from 'react-router-dom';
+
+function Approutes() {
+    return (
+        <>
+        <Roteador>
+     <Routes>
+        <Route exact path='/' Component={Home}/>
+        <Route exact path='/animais' Component={Animais}/>
+        <Route exact path='/fotos' Component={Fotos}/>
+        <Route exact path='/depoimento'Component={Depoimento}/>
+        <Route exact path='/login' Component={Login}/>
+     </Routes>
+     </Roteador>
+        </>
+    )
+}
+export default Approutes;
