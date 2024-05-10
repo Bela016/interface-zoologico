b768dedea2b878ccecc8eab0a667dba98f62e896
bela016 <isabela.seixas2014@gmail.com>
2024-05-10 10:49:15 -0300

    tabela animal

--- src/pages/depoimento/Depoimento.jsx
+++ src/pages/depoimento/Depoimento.jsx
@@ -1,13 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import TabelaAnimal from '../../Components/TabelaAnimal/TabelaAnimal';
 import Navegacao from '../../Components/Navegacao/Navegacao';
-import './Depoimento.css'
 
-    function Depoimento() {
-        return(
-            <>
+function Depoimentos() {
+    const [animais, setAnimais] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/listar-aves');
+                if (!response.ok) {
+                    throw new Error('Erro ao buscar servidor');
+                }
+                const listaAnimais = await response.json();
+                setAnimais(listaAnimais);
+            } catch (error) {
+                console.error('Erro: ', error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    return (
+        <>
             <Navegacao />
-            <p>Nada aqui ainda</p> 
-            </>
-        );
-    }
+            <div className='ctn-animais'>
+                {animais.length > 0 ? (
+                    <TabelaAnimal animais={animais} />
+                ) : (
+                    <p>Carregando...</p>
+                )}
+            </div>
+        </>
+    );
+}
 
-export default Depoimento
+export default Depoimentos;
